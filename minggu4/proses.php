5d8d8c11095cb949ff60202e16f4445f2ed6c238
unknown <kiki27smg@gmail.com>
2022-10-03 19:18:27 +0700

    minggu 4

---
+++ minggu4/proses.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title></title>
+</head>
+<body>
+    <br>
+    <br>
+    <center><h1>DATA BERHASIL DIINPUT</h1>
+    <br>
+    <br>
+    <table border="1" width="400">
+        <tr>
+            <td>NIM</td>
+            <td><?php echo $_POST['nim']; ?></td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $_POST['nama']; ?></td>
+        </tr>    
+        <tr>
+            <td>TANGGAL LAHIR</td>
+            <td><?php echo $_POST['tgllhr']; ?></td>
+        </tr>
+        <tr>
+            <td>JENIS KELAMIN</td>
+            <td><?php echo $_POST['gender']; ?></td>
+        </tr>
+        <tr>
+            <td>PROGRAM STUDI</td>
+            <td><?php echo $_POST['progdi']; ?></td>
+        </tr>
+        <tr>
+            <td>BIDANG MINAT</td>
+            <td><?php echo $_POST['bid']; ?></td>
+        </tr>
+        <tr>
+            <td>BERKAS</td>
+            <td><?php echo $_POST['cv']; ?></td>
+        </tr>
+</table>
+</center>
+</body>
+</html>    
+
+         
